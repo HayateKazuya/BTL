d3611896b92616d6c0ec1f1283564d05fd02dd69
Thanh Luan <thanhluando25@gmail.com>
2024-11-26 11:12:44 +0700

    Check git

--- login.php
+++ login.php
@@ -28,18 +28,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->bind_param("s", $email);
         $stmt->execute();
         $result = $stmt->get_result();
-        
+
         // If email exists, check password
         if ($result->num_rows > 0) {
             $user = $result->fetch_assoc();
-            
+
             // Verify the password
             if (password_verify($password, $user['password'])) {
                 // Successful login
                 $_SESSION['user_id'] = $user['id'];
                 $_SESSION['user_name'] = $user['name'];
                 $_SESSION['user_email'] = $user['email'];
-                
+
                 echo "<script>
                         alert('Đăng nhập thành công!!');
                         window.location.href = '#';
@@ -73,4 +73,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 // Close the connection
 $conn->close();
+
 ?>
+
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+    <title>Đăng nhập</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="login.php" method="POST">
+            <div class="form">
+                <span class="title">Đăng nhập</span>
+
+                <div class="input-field">
+                    <input type="email" name="email" placeholder="Email" required>
+                    <i class="uil uil-envelope-alt icon"></i>
+                </div>
+
+                <div class="input-field">
+                    <input type="password" name="password" class="password" placeholder="Mật khẩu" required>
+                    <i class="uil uil-lock icon"></i>
+                </div>
+
+                <div class="checkbox-text">
+                    <div class="checkbox-content">
+                        <input type="checkbox" id="logCheck">
+                        <label for="logCheck" class="text">Ghi nhớ tài khoản</label>
+                    </div>
+                    <a href="forgotpass.html" class="text">Quên mật khẩu</a>
+                </div>
+
+                <div class="input-field button">
+                    <input type="submit" value="Đăng nhập">
+                </div>
+
+                <div class="login-signup">
+                    <span class="text">Bạn chưa có tài khoản?
+                        <a href="signup.html" class="text signup-link">Đăng kí</a>
+                    </span>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
